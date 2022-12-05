eb1138c7597568437ff43bc41f20f5135fb31858
biometricPsychography <cyclinglinguist@gmail.com>
2022-12-04 21:17:29 -0600

    Basic datapoints now displaying

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ PLOT_WIDTH = WIDTH - PADDING;
 PLOT_HEIGHT = HEIGHT - PADDING;
 
 const xScale = d3.scaleTime();
+xScale.range([PADDING, WIDTH - PADDING])
 
 // input domain and range to yScale
 const yScale = d3.scaleLinear([1, 12], [PADDING, PLOT_HEIGHT]);
@@ -20,19 +21,27 @@ fetch(
     .then(data => {
         const svgWrapper = d3.select('#svg-wrapper');
         let monthlyVariance = data.monthlyVariance
-        let itemsPerLine = monthlyVariance.length / 12
-        c({itemsPerLine})
 
+        let yearsObj = {};
+        for (const element of monthlyVariance) {
+
+            yearsObj[element.year] = element.year;
+        }
+        const yearCount = Object.keys(yearsObj).length;
+        const heatMapElementWidth = WIDTH / yearCount;
+        
+        xScale.domain([monthlyVariance[0].year, monthlyVariance[monthlyVariance.length - 1].year]);
 
         svgWrapper
             .selectAll('rect')
             .data(data.monthlyVariance)
             .enter()
             .append('rect')
-            .attr('width', '30')
+            .attr('width', heatMapElementWidth)
             .attr('height', '40')
-            .attr('x', '10')
-            .attr('y', '10')
+            .attr('x', d => xScale(d.year))
+            .attr('y', (d) => yScale(d.month))
+            .attr('fill', d3.color('steelblue'))
 
     })
 
