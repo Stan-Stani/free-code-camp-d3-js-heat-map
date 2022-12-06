280d2b75fec8849d652c28eac83880ac31b473c5
biometricPsychography <cyclinglinguist@gmail.com>
2022-12-05 19:55:28 -0600

    Get rudimentary colors working

--- src/index.js
+++ src/index.js
@@ -13,13 +13,17 @@ xScale.range([PADDING, WIDTH - PADDING])
 // input domain and range to yScale
 const yScale = d3.scaleLinear([1, 12], [PADDING, PLOT_HEIGHT]);
 
+const svgWrapper = d3.select('#svg-wrapper');
+
+
+
 fetch(
     "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json",
     {method: "GET", header: "Content-Type: application/json"}
 )
     .then(res => res.json())
     .then(data => {
-        const svgWrapper = d3.select('#svg-wrapper');
+        
         let monthlyVariance = data.monthlyVariance
 
         let yearsObj = {};
@@ -32,6 +36,35 @@ fetch(
         
         xScale.domain([monthlyVariance[0].year, monthlyVariance[monthlyVariance.length - 1].year]);
 
+
+
+        let coldestVariance = d3.min(monthlyVariance, element => element.variance);
+        let hottestVariance = d3.max(monthlyVariance, element => element.variance);
+        c({coldestVariance, hottestVariance})
+        // { coldestVariance: -6.976, hottestVariance: 5.228 }
+        let justVarianceArr = monthlyVariance.map(element => {
+            return element.variance;
+        })
+
+
+
+        const colorScale = d3.scaleQuantile()
+        colorScale.domain(justVarianceArr);
+        colorScale.range([
+            d3.color('#005f73'),
+            d3.color('#0a9396'), 
+            d3.color('#94d2bd'), 
+            d3.color('#e9d8a6'),
+            d3.color('#ee9b00'),
+            d3.color('#ca6702'),
+            d3.color('#bb3e03'),
+            d3.color('#ae2012'),
+            d3.color('#9b2226')
+        ])
+
+        c(colorScale.quantiles())
+        
+
         svgWrapper
             .selectAll('rect')
             .data(data.monthlyVariance)
@@ -41,10 +74,43 @@ fetch(
             .attr('height', '40')
             .attr('x', d => xScale(d.year))
             .attr('y', (d) => yScale(d.month))
-            .attr('fill', d3.color('steelblue'))
+            .attr('fill', d => {
+                return colorScale(d.variance).formatHex()
+            }).on('mouseover', (e, d) => {
+                tooltip.select('text')
+                .text(JSON.stringify(d))
+            })
 
+
+        tooltip = buildTooltipScaffold();
+        tooltip.attr('style', 'transform: translate(0px, 500px);')
     })
 
+
+
+
+    function buildTooltipScaffold() {
+        let tooltip = svgWrapper
+            .append('g')
+            .attr('id', 'tooltip')
+        
+        let tooltipRect = tooltip
+            .append('rect')
+            .attr('id', 'tooltip')
+            .attr('rx', '.75%')
+            .attr('ry', '.75%')
+            .attr('width', '400')
+            .attr('height', '100')
+        
+        tooltip
+            .append('text')
+            .text('hello')
+            .attr('x', '0')
+            .attr('y', '32')
+            
+
+        return tooltip;
+    }
 
 
 // utility functions
