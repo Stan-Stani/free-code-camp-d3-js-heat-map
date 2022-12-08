05b52aadf3080fc53421c2a029593c3d2919da9d
biometricPsychography <cyclinglinguist@gmail.com>
2022-12-08 13:16:15 -0600

    Feature: Add button that toggles palette. Add fCC's palette and increase number of colors in palettes to match fCC's example, thereby increasing nuance of heat-map.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // TODO: Revert datum highlighting to black stroke
 // TODO: Correctly align highlighting rect to hovered datum
 // TODO: Clean up top axis first and last ticks, etc.
+// Hide outline when mouse out
 
 // Define global variables
 const WIDTH = 1366;
@@ -21,10 +22,12 @@ const yScale = d3.scaleLinear([1, 13], [PADDING, PLOT_HEIGHT]);
 const DATUM_HEIGHT = yScale(2) - PADDING;
 let datumWidth;
 
-const svgWrapper = d3.select('#svg-wrapper');
+const svgWrapper = d3.select('#svg-wrapper')
+    .attr('viewBox', `0 0 ${WIDTH} ${HEIGHT}`);
 
 const state = {
-    colorScaleIndex: 0
+    colorScaleIndex: 0,
+    paletteIndex: 0
 };
 
 
@@ -62,29 +65,52 @@ fetch(
         let justVarianceArr = monthlyVariance.map(element => {
             return element.variance;
         })
-        let colorPaletteArr = [
-            d3.color('#005f73'),
-            d3.color('#0a9396'), 
-            d3.color('#94d2bd'), 
-            d3.color('#e9d8a6'),
-            d3.color('#ee9b00'),
-            d3.color('#ca6702'),
-            d3.color('#bb3e03'),
-            d3.color('#ae2012'),
-            d3.color('#9b2226')
-        ];
+
+        palettesArr = [
+            {
+                name: "Stan's Palette",
+                colors: [
+                    d3.color('#004352'),
+                    d3.color('#005f73'),
+                    d3.color('#0a9396'), 
+                    d3.color('#94d2bd'), 
+                    d3.color('#e9d8a6'),
+                    d3.color('#ee9b00'),
+                    d3.color('#ca6702'),
+                    d3.color('#bb3e03'),
+                    d3.color('#ae2012'),
+                    d3.color('#9b2226'),
+                    d3.color('#7a292c')
+                ]
+            },
+            {   name: "freeCodeCamp's Palette",
+                colors: [
+                    '#a50026',
+                    '#d73027',
+                    '#f46d43',
+                    '#fdae61',
+                    '#fee090',
+                    '#ffffbf',
+                    '#e0f3f8',
+                    '#abd9e9',
+                    '#74add1',
+                    '#4575b4',
+                    '#313695'
+                ].map(element => d3.color(element)).reverse()
+            }
+        ]
+
+       
 
 
         const colorScalesObj = {};
         colorScalesObj.quantile = d3.scaleQuantile();
         colorScalesObj.quantile.domain(justVarianceArr);
-        colorScalesObj.quantile.range(colorPaletteArr);
-
-        // c(colorScalesObj.quantile.quantiles())
+        colorScalesObj.quantile.range(palettesArr[state.paletteIndex].colors);
 
         colorScalesObj.quantize = d3.scaleQuantize();
         colorScalesObj.quantize.domain([coldestVariance, hottestVariance]);
-        colorScalesObj.quantize.range(colorPaletteArr);
+        colorScalesObj.quantize.range(palettesArr[state.paletteIndex].colors);
 
         colorScalesArr = [
             colorScalesObj.quantile, 
@@ -112,7 +138,8 @@ fetch(
                 datumOutline.attr('x', this.getAttribute('x'))
                 datumOutline.attr('y', this.getAttribute('y'))
                 datumOutline.attr('style', 'visibility: visible;')
-                datumOutline.attr('stroke', (invertColorHexCode(this.getAttribute('fill'))));
+                datumOutline.attr('stroke', 'black');
+
 
                 tooltip.select('text')
                 .text(JSON.stringify(d))
@@ -128,9 +155,9 @@ fetch(
 
         tooltip = buildTooltipScaffold();
 
-       let [button] = [...buildButtons()];
+       let [cycleScaleTypeButton, cyclePaletteButton] = [...buildButtons()];
 
-        button.on('mouseup', (e) => {
+        cycleScaleTypeButton.on('mouseup', (e) => {
 
             state.colorScaleIndex++;
             (state.colorScaleIndex > colorScalesArr.length - 1) ? state.colorScaleIndex = 0 : null;
@@ -141,9 +168,27 @@ fetch(
                 .attr('x', d => xScale(new Date(d.year, 0)))
                 .attr('y', (d) => yScale(d.month))
                 .attr('fill', d => {
-                    return colorScalesArr[state.colorScaleIndex](d.variance).formatHex()
+                    return colorScalesArr[state.colorScaleIndex](d.variance).formatHex();
                 })
         });
+
+        cyclePaletteButton.on('mouseup', e => {
+            state.paletteIndex++;
+            (state.paletteIndex > palettesArr.length - 1) ? state.paletteIndex = 0 : null;
+
+            colorScalesArr.forEach(scale => {
+                scale.range(palettesArr[state.paletteIndex].colors);
+            })
+
+            svgWrapper
+            .selectAll('rect')
+            .data(data.monthlyVariance)
+            .attr('x', d => xScale(new Date(d.year, 0)))
+            .attr('y', (d) => yScale(d.month))
+            .attr('fill', d => {
+                return colorScalesArr[state.colorScaleIndex](d.variance).formatHex();
+            })
+        })
 
         
         let datumOutline = buildDatumOutline()
@@ -178,29 +223,51 @@ function buildTooltipScaffold() {
 }
 
 function buildButtons() {
-    let nextColorScaleButton = 
+    let cycleScaleTypeButton = 
     svgWrapper
         .append('g')
-            .attr('id', 'next-color-scale-button')
+            .attr('id', 'cycle-scale-type-button')
             .attr('class', 'svg-rect-button')
         
-    nextColorScaleButton
+    cycleScaleTypeButton
         .attr('style', `transform: translate(${WIDTH - PADDING - 30}px, 30px`)
     
-    nextColorScaleButton
+    cycleScaleTypeButton
         .append('rect')
             .attr('rx', '.75%')
             .attr('ry', '.75%')
             .attr('width', '60')
             .attr('height', '30')
 
-    nextColorScaleButton
+    cycleScaleTypeButton
         .append('text')
-            .text('hello')
+            .text('scale type')
             .attr('x', '0')
             .attr('y', '15')
+
     
-    return [nextColorScaleButton];
+    let cyclePaletteButton =
+        svgWrapper.append('g')
+            .attr('id', 'cycle-scale-type-button')
+            .attr('class', 'svg-rect-button')
+            
+    cyclePaletteButton
+        .attr('style', `transform: translate(${WIDTH - PADDING - 90}px, 30px`)
+
+    cyclePaletteButton
+        .append('rect')
+            .attr('rx', '.75%')
+            .attr('ry', '.75%')
+            .attr('width', '60')
+            .attr('height', '30')
+
+    cyclePaletteButton
+        .append('text')
+            .text('palette')
+            .attr('x', '0')
+            .attr('y', '15')
+
+    return [cycleScaleTypeButton, cyclePaletteButton];
 }
 
 function buildAxes() {
@@ -263,21 +330,7 @@ function buildDatumOutline() {
     return outlineRect;
 }
 
-function invertColorHexCode(str) {
-    str = str.slice(1);
-    let origColorInt = Number('0x' + str)
-    let largestColorInt = 0xFFFFFF;
 
-    let invertedColorInt = largestColorInt - origColorInt;
-    invertedColorCode = invertedColorInt.toString(16);
-
-    invertedColorCode = '#' + invertedColorCode;
-
-    return invertedColorCode;
-    
-    
-
-}
 
 
 
